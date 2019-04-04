35e28f914a17dd511f241b78a84a4b6fd9d1819f
liuchonghui <liuchonghui@xiaomi.com>
2019-04-04 16:13:48 +0800

    commit changes.
    N/A
    
    Signed-off-by: liuchonghui <liuchonghui@xiaomi.com>

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"log"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-		log.Printf("call main()")
+	log.Printf("call main()")
+	r := setupRouter()
+	// Listen and Server in 0.0.0.0:9090
+	r.Run(":9090")
+}
+
+func setupRouter() *gin.Engine {
+	// Disable Console Color
+	// gin.DisableConsoleColor()
+	r := gin.Default()
+
+	// Ping test
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	// TODO
+
+	return r
 }
